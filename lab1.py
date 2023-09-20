46a9785ac39dc08a470c35c143c3656667b8d27f
Yevhen Kaznovskyi <kaznovsk@cs.ualberta.ca>
2023-09-19 19:54:11 -0600

    push new version

--- lab1.py
+++ lab1.py
@@ -4,3 +4,7 @@ import requests
 
 get_google = requests.get("https://www.google.com")
 print(get_google)
+
+get_me = requests.get("https://raw.githubusercontent.com/yevheniy73/cmput404/lab1/lab1.py")
+
+print(get_me.text)
